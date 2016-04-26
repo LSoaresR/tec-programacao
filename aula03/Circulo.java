0acdaa75f36599d3b718a058eb535295e6a1bad6
Lucas Soares <lucasitachii@hotmail.com>
2016-04-26 18:41:05 -0300

    aula03
    aula03

---
+++ aula03/Circulo.java
@@ -0,0 +1,52 @@
+
+public class Circulo extends Figura implements FiguraColorida {
+    static final double pi = 3.14;
+
+    private double raio;
+    private String tipo = "não colorido";
+    private String cor;
+    
+    //Construtor
+    Circulo() {}
+    Circulo(String cor) {
+        this.tipo = "Colorido";
+        this.cor = cor;
+    }
+
+    public String toString() {
+    	return "Circulo, "+this.tipo;
+    }
+//******************************Desenho()***************************************
+    public void desenhar() {
+    	System.out.println(this.toString());
+    }
+    
+    public void desenhar(String cor) {
+       this.tipo = "Colorido";
+       this.cor = cor;
+       System.out.println(this.toString());
+    }
+//******************************************************************************
+    public void calcularArea() {
+    	 System.out.println("Area é: "+ pi *( raio*raio));
+    } 
+
+    //Metodos gets e sets
+
+    public void  setRaio(int raio) {
+        this.raio = raio;
+    } 
+
+    public double getRaio() {
+        return this.raio;
+    }
+    public String getTipo() {
+        return this.tipo;
+    }
+    
+    public String getCor() {
+        return this.cor;
+    }
+   
+    
+}
---
+++ aula03/Main.java
@@ -0,0 +1,106 @@
+
+public class Main {
+    public static void main(String[] args) {
+          //QUESTAO 03	
+	  //Falta fazer mover os pontos de uma figura 	
+      Quadrado d1 = new Quadrado();
+      Quadrado d2 = new Quadrado();
+
+      //Parte reservada para os testes dos dois quadrados
+
+      // ****************************************************************************
+        
+      
+      d2.desenhar("verde");
+      d1.setLado(3);
+      d1.calcularArea();
+      //d1.mover(1,1);
+
+
+      //*****************************************************************************
+
+      Circulo cc1 = new Circulo();
+      Circulo cc2 = new Circulo();
+
+      //Parte reservada para os testes dos dois circulos
+
+      //*****************************************************************************
+
+      cc1.desenhar();
+      cc1.setRaio(1);
+      cc2.setRaio(2);
+      cc1.calcularArea();
+      cc2.calcularArea();
+
+
+
+      //*****************************************************************************
+      
+      //05 QUESTAO
+      /*
+      Criar um vetor de Figuras
+      */
+      
+      Figura figuras[] = new Figura[10];
+      
+      //figuras para armazenar
+      
+      Quadrado q1 = new Quadrado();
+      Quadrado q2 = new Quadrado("blue");
+      Quadrado q3 = new Quadrado("green");
+      Quadrado q4 = new Quadrado();
+      Quadrado q5 = new Quadrado();
+      
+      Circulo c1 = new Circulo("magenta");
+      Circulo c2 = new Circulo();
+      Circulo c3 = new Circulo("red");
+      Circulo c4 = new Circulo("gray");
+      Circulo c5 = new Circulo();
+      
+      //Preencher lados
+      
+      q1.setLado(1);
+      q2.setLado(2);
+      q3.setLado(3);
+      q4.setLado(4);
+      q5.setLado(5);
+      
+      c1.setRaio(1);
+      c2.setRaio(2);
+      c3.setRaio(3);
+      c4.setRaio(4);
+      c5.setRaio(5);
+      
+      //Preencendo vetor de Figura[]
+      
+      figuras[0] = q1;      
+      figuras[1] = q2;
+      figuras[2] = q3;
+      figuras[3] = q4;
+      figuras[4] = q5;
+      figuras[5] = c1;
+      figuras[6] = c2;
+      figuras[7] = c3;
+      figuras[8] = c4;
+      figuras[9] = c5;
+      
+      Quadrado aux;
+      Circulo aux2;
+      
+      for(int i = 0; i < figuras.length; i++) {
+           if(figuras[i] instanceof Quadrado) {
+               aux = (Quadrado)figuras[i];
+               if(aux.getTipo() == "Colorido") {
+                  aux.calcularArea();
+               }
+               
+           }else if(figuras[i] instanceof Circulo) {
+               aux2 = (Circulo)figuras[i];
+               if(aux2.getTipo() == "Colorido") {
+                   aux2.calcularArea();
+               }
+           }
+      }
+      
+    }
+}
