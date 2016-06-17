c7698018955515aa565cdbe255005b4f405a3609
Lucas Soares <lucasitachii@hotmail.com>
2016-06-17 18:03:48 -0300

    aula06

---
+++ aula06/Teste_Hash2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Teste_Hash2 {
+    
+ public static void listar(Collection c) {
+     Iterator it = c.iterator();
+     
+     while(it.hasNext()) {
+       System.out.println(it.next());
+     }
+ }   
+ 
+ public static void listar2(Collection c) {
+    System.out.println("*** Listar 2 ***");
+    for(Object o: c)
+        System.out.println(o);
+ }
+ 
+ public static void main(String args[]){
+    //1º 
+    //Set<String> mesesHash = new HashSet<>();
+    //2º
+    //Set<String> mesesHash = new TreeSet<>();
+    //3º
+    Set<String> mesesHash = new LinkedHashSet<>();
+    
+    mesesHash.add("Janeiro");
+    mesesHash.add("Fevereiro");
+    mesesHash.add("Março");
+    mesesHash.add("Abril");
+    mesesHash.add("Maio");
+    mesesHash.add("Fevereiro");
+    
+    System.out.println("HashSet:");
+    Teste_Hash2.listar(mesesHash);
+    Teste_Hash2.listar2(mesesHash);
+ }
+}
